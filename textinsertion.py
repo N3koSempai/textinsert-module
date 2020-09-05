27602531a079a41bfb826fbcf4828640f0211e3f
N3koSempai <alvaarom12098@gmail.com>
2020-09-05 21:10:44 +0200

    now is a simply funtion with three arguments

--- textinsertion.py
+++ textinsertion.py
@@ -2,46 +2,39 @@
 """
 Created on Thu Sep  3 07:09:39 2020
 
-@author: n3ko
+@author: N3koSempai
 """
 
 from io import open
 
-class Name():
-    
-    def __init__(self):
-        self.initialcaract= 0
-        self.myline=""
-        self.urlfile=""
-        self.f=""
-        
-    def textinsert(self, initialcaract, myline, urlfile):
-#initialcaracter is the caracter for start
-#myline is a custom line of text. remember the initial space and finish space
-#  urlfile is the direction in windows example E:/ejem/ejem.txt
-    
-    
-        archivo=open(urlfile, "r+")
 
 
-        archivo.seek(0) 
-        
-        self.f=archivo.read(initialcaract)
-        ftwo=initialcaract+1
-      
-        xtra=archivo.readlines(ftwo)
-        stra= "".join(xtra)
+def textinsert(initialcaract, myline, urlfile):
+    """initialcaracter is the caracter for start.
+myline is your custom line of text you need add in the midle of text file.
+remember the initial space and finish space.
+urlfile is the direction in windows example E:/ejem/ejem.txt
+this funtion create a second file whit mod after normal name"""
+
+
+    archivo = open(urlfile, "r")
+
+
+    archivo.seek(0)
+
+    first = archivo.read(initialcaract)
+
+    xtra = archivo.read()
+    stra = "".join(xtra)
 #the mod archive finished in mod
-        file=urlfile + "mod"
+    file = urlfile + "mod"
 
 #creation of new file and writing
-        newfile=open(file, "w")
+    newfile = open(file, "w")
 
-        newfile.write(self.f)
-        newfile.write(myline)
-        newfile.write(stra)
-    
-        archivo.close()
-        newfile.close()
-    
-    
+    newfile.write(first)
+    newfile.write(myline)
+    newfile.write(stra)
+
+    archivo.close()
+    newfile.close()
